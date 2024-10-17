ae719c98ac2f6627d71ed1bb54986d274861bd7d
Milan <47188377+milanlakhani@users.noreply.github.com>
2024-10-17 15:46:58 +0100

    Second round of requirements review (#25)
    * Add requirements
    
    Signed-off-by: Milan Lakhani <milan.l@hotmail.co.uk>
    
    * Add help to gen_reqs, allign reqs styles
    
    Signed-off-by: Milan Lakhani <milan.l@hotmail.co.uk>
    
    * Update script, remove full stops, make changes to spreadsheet
    
    Signed-off-by: Milan Lakhani <milan.l@hotmail.co.uk>
    
    * Address github suggestion
    
    Signed-off-by: Milan Lakhani <milan.l@hotmail.co.uk>
    
    * Add full stops to Level 2 requirements
    
    Signed-off-by: Milan Lakhani <milan.l@hotmail.co.uk>
    
    * Make review changes to L2 reqs
    
    Signed-off-by: Milan Lakhani <milan.l@hotmail.co.uk>
    
    * Make review change
    
    Signed-off-by: Milan Lakhani <milan.l@hotmail.co.uk>
    
    * Remove done TODO
    
    Signed-off-by: Milan Lakhani <milan.l@hotmail.co.uk>
    
    * Change should to shall
    
    Signed-off-by: Milan Lakhani <milan.l@hotmail.co.uk>
    
    * Change should to shall
    
    Signed-off-by: Milan Lakhani <milan.l@hotmail.co.uk>
    
    * Update l1_reqs.txt
    
    Signed-off-by: Shefali Sharma <sshefali021@gmail.com>
    
    * Update l2_reqs.txt
    
    Signed-off-by: Shefali Sharma <sshefali021@gmail.com>
    
    * Apply suggestions from code review
    
    Polishing of L1 requirements
    
    Signed-off-by: Milan Lakhani <milan.l@hotmail.co.uk>
    
    Co-authored-by: Nicole Pappler <nicole@alektometis.com>
    
    * Apply suggestions from code review
    
    Correct requirement.
    
    Signed-off-by: Milan Lakhani <milan.l@hotmail.co.uk>
    
    * Update applying-stpa/l1_reqs.txt
    
    Signed-off-by: Milan Lakhani <milan.l@hotmail.co.uk>
    
    * Update applying-stpa/l2_reqs.txt
    
    Co-authored-by: Milan <47188377+milanlakhani@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Requirements refactor
    
    Signed-off-by: Milan Lakhani <milan.l@hotmail.co.uk>
    
    * Update applying-stpa/l1_reqs.txt
    
    * Update applying-stpa/l1_reqs.txt
    
    Signed-off-by: Milan Lakhani <milan.l@hotmail.co.uk>
    
    Co-authored-by: Kate Stewart <13152682+kestewart@users.noreply.github.com>
    
    * Update applying-stpa/l1_reqs.txt
    
    Signed-off-by: Milan Lakhani <milan.l@hotmail.co.uk>
    
    Co-authored-by: Kate Stewart <13152682+kestewart@users.noreply.github.com>
    
    * Update applying-stpa/l1_reqs.txt
    
    Signed-off-by: Milan Lakhani <milan.l@hotmail.co.uk>
    
    Co-authored-by: Kate Stewart <13152682+kestewart@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Requirement updates
    
    Co-authored-by: Kate Stewart <13152682+kestewart@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Signed-off-by: Milan Lakhani <milan.l@hotmail.co.uk>
    
    Co-authored-by: Kate Stewart <13152682+kestewart@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Signed-off-by: Milan Lakhani <milan.l@hotmail.co.uk>
    
    Co-authored-by: Kate Stewart <13152682+kestewart@users.noreply.github.com>
    
    * Update applying-stpa/l1_reqs.txt
    
    Signed-off-by: Milan Lakhani <milan.l@hotmail.co.uk>
    
    Co-authored-by: Kate Stewart <13152682+kestewart@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Milan Lakhani <milan.l@hotmail.co.uk>
    Signed-off-by: Shefali Sharma <sshefali021@gmail.com>
    Co-authored-by: Shefali Sharma <sshefali021@gmail.com>
    Co-authored-by: Nicole Pappler <nicole@alektometis.com>
    Co-authored-by: Kate Stewart <13152682+kestewart@users.noreply.github.com>

---
+++ applying-stpa/gen_requirements.py
@@ -0,0 +1,51 @@
+# SPDX-FileCopyrightText: 2023 ELISA <medical-devices@lists.elisa.tech>
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+import yaml
+import pandas as pd
+import click
+import pprint
+from functools import cmp_to_key
+
+filter_out_list = ["Covered", "---"]
+
+def compare_items(item1, item2):
+    item1_number = int(item1[0].split("-")[-1])
+    item2_number = int(item2[0].split("-")[-1])
+    return item1_number - item2_number
+
+@click.command()
+@click.option('--xlsx_file', help='OpenAPS STPA spreadsheet file')
+@click.option('--loss_scenarios_sheet', help='Sheet in OpenAPS STPA spreadsheet file, eg "Level 1 Loss Scenario Analysis"')
+@click.option('--output_file', help='Output txt file')
+def gen_requirements(xlsx_file, loss_scenarios_sheet, output_file):
+    scenario_df = pd.read_excel(xlsx_file, engine='openpyxl', sheet_name=loss_scenarios_sheet)
+    requirement_columns = 0
+    all_requirements = {}
+    identifier = 1
+
+    for row in scenario_df.iterrows():
+        data = row[1]
+        In = False
+        for i in range(1, 9):
+            if f"Requirement {str(i)}" in data and type(data[f"Requirement {str(i)}"]) is str:
+                requirements = data[f"Requirement {str(i)}"].split("\n")
+                for requirement in requirements:
+                    if requirement:
+                        if requirement[-1] != ".":
+                    	    requirement += "."
+                        if requirement not in all_requirements.values() and requirement != "-":
+                            if all(word not in requirement for word in filter_out_list):
+                                all_requirements[f"REQ-{identifier}"] = requirement
+                                identifier += 1
+
+    req_string = "\n".join([": ".join(req) for req in sorted(all_requirements.items(), key=cmp_to_key(compare_items))])
+
+    with open(output_file, "w") as file:
+        file.write(req_string)
+    pprint.pprint(sorted(all_requirements.items(), key=cmp_to_key(compare_items)), width=800)
+
+    return req_string
+
+if __name__ == '__main__':
+    gen_requirements()
